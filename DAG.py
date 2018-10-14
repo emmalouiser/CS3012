fc17d751ea5ea2efbc8977ff7eb46d198bda9b2d
Emma-Louise <ruaneem@tcd.ie>
2018-10-14 15:28:45 +0100

    Working LCA Algorithm Implemented

--- DAG.py
+++ DAG.py
@@ -2,7 +2,7 @@ class Graph(object):
 
     def __init__(self, graph_dict=None):
         if graph_dict == None:
-            graph_dict = {}
+            graph_dict = set({})
         self.__graph_dict = graph_dict
 
     def vertices(self):
@@ -55,44 +55,11 @@ class Graph(object):
             res += str(edge) + " "
         return res
 
+    def dfs_recursive(self, vertex, path):
+        path += [vertex]
 
-if __name__ == "__main__":
+        for neighbor in self.__graph_dict[vertex]:
+            if neighbor not in path:
+                path = self.dfs_recursive(neighbor, path)
 
-    g = { "a" : ["d"],
-          "b" : ["c"],
-          "c" : ["b", "c", "d", "e"],
-          "d" : ["a", "c"],
-          "e" : ["c"],
-          "f" : []
-        }
-
-
-    graph = Graph(g)
-
-    print("Vertices of graph:")
-    print(graph.vertices())
-
-    print("Edges of graph:")
-    print(graph.edges())
-
-    print("Add vertex:")
-    graph.add_vertex("z")
-
-    print("Vertices of graph:")
-    print(graph.vertices())
-
-    print("Add an edge:")
-    graph.add_edge({"a","z"})
-
-    print("Vertices of graph:")
-    print(graph.vertices())
-
-    print("Edges of graph:")
-    print(graph.edges())
-
-    print('Adding an edge {"x","y"} with new vertices:')
-    graph.add_edge({"x","y"})
-    print("Vertices of graph:")
-    print(graph.vertices())
-    print("Edges of graph:")
-    print(graph.edges())
+        return path
