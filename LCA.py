11870102cf2dc540821da78a0f9dcb309004e9ef
Emma-Louise <ruaneem@tcd.ie>
2018-10-21 00:35:47 +0100

    Tests on LCA

--- LCA.py
+++ LCA.py
@@ -1,6 +1,14 @@
-from DAG import Graph
 import sys
 sys.dont_write_bytecode = True
+
+from DAG import Graph
+import unittest
+from sys import stdout
+from coverage import Coverage
+
+cov = Coverage()
+cov.set_option('report:show_missing', True)
+cov.start()
 
 def LCA( graph, a, b):
     dfs_a = graph.dfs_recursive(a, [])
@@ -12,14 +20,26 @@ def LCA( graph, a, b):
     else:
         return -1;
 
-g = {   1: [2, 3],
-        2: [4, 5],
-        3: [5],
-        4: [6],
-        5: [6],
-        6: [7],
-        7: []}
+class TestStringMethods(unittest.TestCase):
 
-graph = Graph(g)
+    def test_LCA_basic(self):
+        g = {   1: [2, 3],
+                2: [4, 5],
+                3: [5],
+                4: [6],
+                5: [6],
+                6: [7],
+                7: []}
 
-print(LCA(graph, 4, 5))
+        graph = Graph(g)
+
+        self.assertEqual(LCA(graph, 4, 5), 6)
+
+    def test_null(self):
+        graph = Graph()
+        self.assertEqual(LCA(graph, 4, 5), -1)
+
+unittest.main(exit=False)
+
+cov.stop()
+cov.report()
