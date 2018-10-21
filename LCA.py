c8179c9100bdb27796961a4e9dbbcb80e6e8bc9c
Emma-Louise <ruaneem@tcd.ie>
2018-10-21 01:03:33 +0100

    LCA on vertices not in graph and no LCA existing

--- LCA.py
+++ LCA.py
@@ -15,13 +15,13 @@ def LCA(graph, a, b):
         dfs_a = graph.dfs_recursive(a, [])
         dfs_b = graph.dfs_recursive(b, [])
 
-        for i in range(len(dfs_a)):
+        for i in range(min(len(dfs_a), len(dfs_b))):
           if dfs_a[i] == dfs_b[i]:
               return dfs_a[i]
         else:
             return -1;
 
-    else: 
+    else:
         return -1;
 
 class TestStringMethods(unittest.TestCase):
@@ -38,10 +38,37 @@ class TestStringMethods(unittest.TestCase):
         graph = Graph(g)
 
         self.assertEqual(LCA(graph, 4, 5), 6)
+        self.assertEqual(LCA(graph, 43, 35), -1)
 
     def test_null(self):
         graph = Graph()
         self.assertEqual(LCA(graph, 4, 5), -1)
+
+    def test_no_LCA(self):
+        g = {   1: [2, 3],
+                2: [4, 5],
+                3: [5],
+                4: [6],
+                5: [6],
+                6: [7],
+                7: []}
+
+        graph = Graph(g)
+
+        self.assertEqual(LCA(graph, 1, 7), -1)
+
+    def test_vertices_not_in_graph(self):
+        g = {   1: [2, 3],
+                2: [4, 5],
+                3: [5],
+                4: [6],
+                5: [6],
+                6: [7],
+                7: []}
+
+        graph = Graph(g)
+
+        self.assertEqual(LCA(graph, 43, 35), -1)
 
 unittest.main(exit=False)
 
